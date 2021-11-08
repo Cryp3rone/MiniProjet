da75787357fbd7d3235546818215ecaa79a16222
AngelSushi <angel666.yt@gmail.com>
2021-11-08 17:09:52 +0100

    Add SMFL Lib

--- Jeu2D/Jeu2D/Jeu2D.cpp
+++ Jeu2D/Jeu2D/Jeu2D.cpp
@@ -1,4 +1,23 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Jeu2D");
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			switch (event.type) {
+				case sf::Event::Closed:
+					window.close();
+					break;
+				
+			}
+		}
+		window.clear();
+
+		// DRAW SECTION
+
+		window.display();
+	}
 }
