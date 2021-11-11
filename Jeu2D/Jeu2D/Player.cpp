4f632dd6a299ac824614400367b26151a60fbbcb
Amaury <amaury.mehlman@gmail.com>
2021-11-11 14:50:01 +0100

    Merge remote-tracking branch 'origin/Amaury' into Amaury

--- Jeu2D/Jeu2D/Player.cpp
+++ Jeu2D/Jeu2D/Player.cpp
@@ -1,5 +1,7 @@
 #include "Player.h"
 #include <SFML/Graphics.hpp>
+#include "LevelGenerator.h"
+#include "Collision.h"
 
 Player newPlayer()
 {
@@ -7,12 +9,14 @@ Player newPlayer()
 	p.body = sf::CircleShape(20.f);
 	p.body.setFillColor(sf::Color::Red);
 	p.body.setOrigin(p.body.getPosition().x + p.body.getRadius(), p.body.getPosition().y + p.body.getRadius());
+	p.collision = sf::FloatRect(p.body.getPosition().x, p.body.getPosition().y,p.body.getPosition().x + p.body.getScale().x,p.body.getPosition().y + p.body.getScale().y);
 	p.health = 100.f;
 	return p;
 }
 
-void MovePlayer(Player& player, float dt, sf::Vector2f& velocity, sf::View& view)
+void MovePlayer(Player& player, float dt, sf::Vector2f& velocity, sf::View& view,World* world)
 {
+	// MOVEMENT
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
 		player.body.move(sf::Vector2f(0.f, -speed * dt));
 		view.move(sf::Vector2f(0.f, -speed * dt));
@@ -23,14 +27,35 @@ void MovePlayer(Player& player, float dt, sf::Vector2f& velocity, sf::View& view
 	}
 	
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		player.body.move(sf::Vector2f(-speed * dt, 0.f));
-		view.move(sf::Vector2f(-speed * dt, 0.f));
+		player.direction = sf::Vector2f(-1.f, 0.f);
+
+		if (player.direction.x != player.lastDirection.x || (player.direction.x == player.lastDirection.x && player.mooveX)) { // On regarde si le joueur change de direction ou si il est dans la même direction et qu'il peut se déplacer
+			player.body.move(sf::Vector2f(-speed * dt, 0.f));
+			view.move(sf::Vector2f(-speed * dt, 0.f));
+			
+			if (player.direction.x != player.lastDirection.x) {
+				player.mooveX = true;
+				player.lastDirection = player.direction;
+			}
+		}
 	}
 	
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		player.body.move(sf::Vector2f(speed * dt, 0.f));
-		view.move(sf::Vector2f(speed * dt, 0.f));
+		player.direction = sf::Vector2f(1.f, 0.f);
+
+		if (player.direction.x != player.lastDirection.x || (player.direction.x == player.lastDirection.x && player.mooveX))  { // On regarde si le joueur change de direction ou si il est dans la même direction et qu'il peut se déplacer
+			player.body.move(sf::Vector2f(speed * dt, 0.f));
+			view.move(sf::Vector2f(speed * dt, 0.f));
+			
+			if (player.direction.x != player.lastDirection.x) {
+				player.mooveX = true;
+				player.lastDirection = player.direction;
+			}
+		}
 	}
+
+
+	// JUMP
 
 	if (isGrounded(player))
 	{
@@ -51,6 +76,25 @@ void MovePlayer(Player& player, float dt, sf::Vector2f& velocity, sf::View& view
 	{
 		player.body.setPosition(player.body.getPosition().x, groundY - player.body.getRadius());
 	}
+
+
+	// COLLISION
+	for (sf::RectangleShape rectangle : world->rectangles) {
+		if (rectangle.getGlobalBounds().intersects(player.body.getGlobalBounds())) 
+			OnCollision(player, rectangle, false);
+
+		for (Ennemy ennemy : world->ennemies) {
+			if (ennemy.circle != nullptr) {
+				if (ennemy.circle->getGlobalBounds().intersects(player.body.getGlobalBounds()))
+					OnCollision(player, *(ennemy).circle, true);
+			}
+			else {
+				if (ennemy.rectangle->getGlobalBounds().intersects(player.body.getGlobalBounds()))
+					OnCollision(player, *(ennemy).rectangle, true);
+			}
+		}
+	}
+
 }
 
 bool isGrounded(Player& p)
--- Jeu2D/Jeu2D/Player.h
+++ Jeu2D/Jeu2D/Player.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "LevelGenerator.h"
 
 const float groundY = 400.f;
 const float speed = 400.f;
@@ -10,10 +11,14 @@ struct Player
 {
 	sf::CircleShape body;
 	float health;
+	sf::FloatRect collision;
+	bool mooveX;
+	sf::Vector2f direction;
+	sf::Vector2f lastDirection;
 };
 
 Player newPlayer();
 
-void MovePlayer(Player&, float, sf::Vector2f&,sf::View& view);
+void MovePlayer(Player&, float, sf::Vector2f&,sf::View& view,World* world);
 
 bool isGrounded(Player&);
